70da7e5b4a9474904066cc35ab46430f1d4d59a3
gilomandam <maxxam0010@gmail.com>
2023-04-21 18:47:21 +0800

    initial comiit

---
+++ jsfunction.js
@@ -0,0 +1,27 @@
+
+let addDisplayResult = (target, value) => {
+  document.getElementById(target).innerHTML += '<br/>' +  value;
+}
+/*function DivisibleBy7(num) {
+
+  if (num < 0)
+    return isDivisibleBy7(-num);
+
+  if (num == 0 || num == 7)
+    return 1;
+  if (num < 10)
+    return 0;
+
+  return isDivisibleBy7(num / 10 - 2 *
+    (num - num / 10 * 10));
+}
+
+
+let num = 616;
+if (isDivisibleBy7(num) >= 0)
+  document.write("Divisible");
+else
+  document.write("Not Divisible");
+
+*/
+  //function method
